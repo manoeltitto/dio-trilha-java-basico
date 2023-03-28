ec00c89f9eca068f8cf5dd8f532012487232dd1a
manoeltitto <manoeltito@hotmail.com>
2023-03-28 06:54:54 -0300

    Atualizando os códigos de exemplos Java Básico DIO

---
+++ tipo-variaveis-java-basico/tipo-variaveis/src/Operadores.java
@@ -0,0 +1,24 @@
+public class Operadores {
+
+    public static void main(String[] args) throws Exception{ 
+
+
+    int a, b;
+      a = 5;
+      b = 6;
+        String resultado = "";
+
+if (a == b)
+resultado = "verdadeiro";
+else
+resultado = "Falso";
+
+System.out.println(resultado);
+
+a = 6;
+
+resultado = a == b ? "verdadeiro" : "falso";
+System.out.println(resultado);
+
+}    
+}
--- tipo-variaveis-java-basico/tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis-java-basico/tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,26 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+      final  double VALOR_DE_PI = 3.14;
+      int numero = 5;
+      numero = -numero;
+
+
+      String nomeCompleto = "Manoel " + "Titto";
+
+      System.out.println(salarioMinimo);
+      System.out.println(numeroCurto);
+      System.out.println(numeroNormal);
+      System.out.println(nomeCompleto);
+      System.out.println(numero);
+
+      numero = numero*  -1;
+
+      System.out.println(numero);
+
+      
     }
 }
